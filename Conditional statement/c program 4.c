5595f53c14e184daf0326860c215c3631621a098
rekhanagaraj9787-ux <rekhanagaraj9787@gmail.com>
2026-02-18 11:37:55 +0530

    Add files via upload
    temperature

---
+++ Conditional statement/c program 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+        int temp;
+        scanf("%d",&temp);
+        if(temp > 40)
+        printf("High Temperature Alert!");
+        else
+        printf("Temperature is Normal");
+
+
+    return 0;
+}
