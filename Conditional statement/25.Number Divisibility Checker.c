cfb46bb8d527307ef55f7e4407954ee1a68677c2
rekhanagaraj9787-ux <rekhanagaraj9787@gmail.com>
2026-02-28 17:10:29 +0530

    Add files via upload

---
+++ Conditional statement/25.Number Divisibility Checker.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 5 == 0 && num % 11 == 0) {
+        printf("Number is divisible by 5 and 11.\n");
+    } else {
+        printf("Number is not divisible by both.\n");
+    }
+    return 0;
+}
