02fa12f7e0fcaaa5962bd379b3bdb14ff9c68eaf
rekhanagaraj9787-ux <rekhanagaraj9787@gmail.com>
2026-02-28 16:03:48 +0530

    Add files via upload

---
+++ Conditional statement/11.salary.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    float basic, hra, da, gross;
+
+    printf("Enter Basic Salary: ");
+    scanf("%f", &basic);
+
+    if (basic <= 70000) {
+        hra = basic * 0.30;
+        da = basic * 0.80;
+    } else {
+        hra = basic * 0.20;
+        da = basic * 0.50;
+    }
+
+    gross = basic + hra + da;
+
+    printf("HRA = %.2f\n", hra);
+    printf("DA = %.2f\n", da);
+    printf("Gross Salary = %.2f", gross);
+
+    return 0;
+}
