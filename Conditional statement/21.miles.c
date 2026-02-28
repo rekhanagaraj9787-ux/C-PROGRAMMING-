69e4a3d28d6a4001871dc6d9db6830fd054ea92f
rekhanagaraj9787-ux <rekhanagaraj9787@gmail.com>
2026-02-28 16:29:30 +0530

    Add files via upload

---
+++ Conditional statement/21.miles.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float miles, gallons, mpg;
+
+    printf("Enter miles driven: ");
+    scanf("%f", &miles);
+
+    printf("Enter gallons used: ");
+    scanf("%f", &gallons);
+
+    mpg = miles / gallons;
+
+    printf("MPG = %.2f\n", mpg);
+
+    if (mpg < 15)
+        printf("Classification: Poor\n");
+    else if (mpg < 25)
+        printf("Classification: Average\n");
+    else if (mpg < 35)
+        printf("Classification: Good\n");
+    else
+        printf("Classification: Excellent\n");
+
+    return 0;
+}
